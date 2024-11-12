1955a123b6633a2cf85e0ec3e7f60171fba7928b
zlatodima <dmitriy.zlatov1@gmail.com>
2024-11-13 00:31:19 +0100

    fix: Changed db connection message

--- DBConnection.js
+++ DBConnection.js
@@ -12,7 +12,7 @@ const postgresConnection = new Sequelize(
 async function tryToConnect() {
     try {
         await postgresConnection.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('Postgres: Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
